cff42150e09b1762cab73edaee0d9587c0ab49c6
Chuyang Liu <Chuyang Liu>
2016-03-21 17:40:41 +0800

    finish game class

--- TicTacToe/Game.cpp
+++ TicTacToe/Game.cpp
@@ -1,22 +1,114 @@
 #include "Game.h"
+#include "AI.h"
 #include <iostream>
 using namespace std;
 
 void Game::start() {
-    int firstPlace = chooseFirstPlace();
-    if (firstPlace == 1) {  // Choose computer to be the first
-
+    Role firstPlace = chooseFirstPlace();
+    if (firstPlace == COMP) {  // Choose computer to be the first
+        compPlace();
+    }
+    while (1) {
+        printBoard();
+        if (handleGameOver()) break;
+        humanPlace();
+        printBoard();
+        if (handleGameOver()) break;
+        compPlace();
     }
 }
 
-int Game::chooseFirstPlace() {
+Game::Role Game::chooseFirstPlace() {
     int choice;
     while (1) {
         cout << "Who will place the first piece?" << endl;
         cout << "0->Human first" << endl;
         cout << "1->Computer first" << endl;
         cin >> choice;
-        if (choice == 0 || choice == 1) break;
+        if (choice == 0 || choice == 1) {
+            cout << endl;
+            break;
+        }
     }
-    return choice;
+    return (Role)choice;
+}
+
+void Game::printBoard() {
+    board.print();
+}
+
+int Game::getPlacePosition() {
+    string input;
+    while (1) {
+        cout << "It's your turn. Where to place the chess piece?" << endl;
+        cout << "(Typing 22 means place at row 2, column 2):" << endl;
+        cin >> input;
+        if (isLegalPlacePosition(input)) {
+            cout << endl;
+            break;
+        } else {
+            cout << "Your input format is illegal. Please try again." << endl;
+        }
+    }
+    // Change coordinate to one-dimension
+    return convertInputToPosition(input);
+}
+
+bool Game::isLegalPlacePosition(const string &input) {
+    return input.size() == 2 && input[0] >= '1' && input[0] <= '3'
+        && input[1] >= '1' && input[1] <= '3'
+        && board.isEmpty(convertInputToPosition(input));
+}
+
+int Game::convertInputToPosition(const string &input) {
+    return (input[0] - '0' - 1) * 3 + (input[1] - '0' - 1);
+}
+
+void Game::humanPlace() {
+    int pos = getPlacePosition();
+    board.placeHuman(pos);
+    cout << "Your choice:" << endl;
+}
+
+void Game::compPlace() {
+    int bestMove = 0, value = 0;
+    AI::findCompMove(board, bestMove, value);
+    board.placeComp(bestMove);
+    cout << "The computer's choice:" << endl;
+}
+
+bool Game::gameIsOver(bool &draw, Role &win) {
+    if (board.compWin()) {
+        win = COMP;
+        draw = false;
+        return true;
+    } else if (board.humanWin()) {
+        win = HUMAN;
+        draw = false;
+        return true;
+    } else if (board.isFull()) {
+        draw = true;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Game::handleGameOver() {
+    bool draw = false;
+    Role whoWin = HUMAN;
+    if (gameIsOver(draw, whoWin)) {
+        if (draw) {
+            cout << "Draw!" << endl;
+        } else {
+            if (whoWin == COMP) {
+                cout << "You lose!" << endl;
+            } else if (whoWin == HUMAN) {
+                cout << "Congratualtions! You defeat the computer." << endl;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 }
--- TicTacToe/Game.h
+++ TicTacToe/Game.h
@@ -2,8 +2,12 @@
 
 #include "Chessboard.h"
 
+#include <string>
+
 class Game {
 public:
+    enum Role {HUMAN, COMP};
+
     // Start game
     void start();
 private:
@@ -11,5 +15,36 @@ private:
 
     // Choose the one who places the first piece
     // Return 0 means human-first, 1 means computer-first.
-    int chooseFirstPlace();
+    Role chooseFirstPlace();
+
+    // Print chess board
+    void printBoard();
+
+    // Ask user where to place a chess piece
+    // Return the postion to place the chess piece
+    int getPlacePosition();
+
+    // Ask human to place the chess piece
+    void humanPlace();
+
+    // Computer decides where to place the chess piece
+    void compPlace();
+
+    // Judge if game is over
+    // Return true if the game is over, and if @param draw
+    // equals true, the @param win is useless.
+    bool gameIsOver(bool &draw, Role &win);
+
+    // If game is over, handle it
+    // Return true means game is over.
+    bool handleGameOver();
+
+    // Judge if the input in getPlacePosition() is legal.
+    // The input must be a string of length 2.
+    // Input[0] means the row position
+    // Input[1] means the column position
+    bool isLegalPlacePosition(const std::string &input);
+
+    // Convert the user's input to position in one-dimension array;
+    int convertInputToPosition(const std::string &input);
 };
